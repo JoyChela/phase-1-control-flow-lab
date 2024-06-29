1987be7d9e7464a4eae4e95c0287087c2be58200
Joy Chela <joychela000@gmail.com>
2024-06-29 09:58:05 +0300

    control

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let outcome
+  if (feet <= 400){
+    outcome = 'This one is on me!';
+  } else if(feet >=400 && feet <= 2000){
+    outcome = 'That will be twenty bucks.';
+  } else if (feet > 2000 && feet <= 2500){
+    outcome = 'I will gladly take your thirty bucks.';
+  } else {
+    outcome = 'No can do.';
+  }
+  return outcome;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+let ternaryCheckCity = function(city) {
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+};
+
+  
+  function switchOnCharmFromTip(tip) {
+
+    let response;
+
+    switch (tip) {
+      case 'generous':
+        response = 'Thank you so much.';
+        break;
+      case 'not as generous':
+        response = 'Thank you.';
+        break;
+      default:
+        response = 'Bye.';
+        break;
+    }
+    return response;
+  }
